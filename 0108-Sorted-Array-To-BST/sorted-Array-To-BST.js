f346ec62fd8e38fecd8067d52b094185e6ccf53a
liufei <492525217@qq.com>
2020-07-03 09:58:58 +0800

    70,108,125,173

---
+++ 0108-Sorted-Array-To-BST/sorted-Array-To-BST.js
@@ -0,0 +1,26 @@
+// 108. 将有序数组转换为二叉搜索树
+// 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+
+// 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+// 示例:
+
+// 给定有序数组: [-10,-3,0,5,9],
+
+// 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
+
+var sortedArrayToBST = function (nums) {
+    if (!nums.length) return null;
+    const mid = Math.floor(nums.length / 2);
+    const root = new TreeNode(nums[mid]);
+
+    root.left = sortedArrayToBST(nums.slice(0, mid));
+    root.right = sortedArrayToBST(nums.slice(mid + 1))
+    return root;
+}
