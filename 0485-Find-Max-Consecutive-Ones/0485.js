ea5f2c87ca25a44e82633e3a4acaf096e638ba2e
brookliu <492525217@qq.com>
2021-02-15 16:32:01 +0800

    0485

---
+++ 0485-Find-Max-Consecutive-Ones/0485.js
@@ -0,0 +1,27 @@
+// 485. 最大连续1的个数
+// 给定一个二进制数组， 计算其中最大连续1的个数。
+
+// 示例 1:
+
+// 输入: [1,1,0,1,1,1]
+// 输出: 3
+// 解释: 开头的两位和最后的三位都是连续1，所以最大连续1的个数是 3.
+// 注意：
+
+// 输入的数组只包含 0 和1。
+// 输入数组的长度是正整数，且不超过 10,000。
+
+var findMaxConsecutiveOnes = function(nums) {
+  let maxCount = 0, count = 0;
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === 1) {
+      count++;
+    } else {
+      maxCount = Math.max(maxCount, count);
+      count = 0;
+    }
+  }
+  maxCount = Math.max(maxCount, count);
+  return maxCount;
+}
