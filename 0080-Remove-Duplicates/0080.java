b1f02efe82086a5236126daf82725e9cec01bd25
brookliu <492525217@qq.com>
2021-04-06 09:30:45 +0800

    0080

---
+++ 0080-Remove-Duplicates/0080.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int removeDuplicates(int[] nums) {
+    int n = nums.length;
+    if (n <= 2) {
+      return n;
+    }
+    int slow = 2, fast = 2;
+    while (fast < n) {
+      if (nums[slow - 2] != nums[fast]) {
+        nums[slow] = nums[fast];
+        ++slow;
+      }
+      ++fast;
+    }
+    return slow;
+  }
+}
---
+++ 0080-Remove-Duplicates/0080.py
@@ -0,0 +1,8 @@
+class Solution:
+  def removeDuplicates(self, nums: List[int]) -> int:
+    j = 2
+    for i in range(2, len(nums)):
+      if nums[i] != nums[j - 2]:
+        nums[j] = nums[i]
+        j += 1
+    return j
