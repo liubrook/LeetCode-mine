cd778e844b3a582f420aa5223b17299554ec54d8
brookliu <492525217@qq.com>
2021-04-08 09:30:38 +0800

    0153

---
+++ 0153-Find-Min/0153.py
@@ -0,0 +1,11 @@
+class Solution:
+  def finMin(self, nums: List[int]) -> int:
+    low, high = 0, len(nums) - 1
+    while low < high:
+      mid = low + (high - low) // 2
+      if nums[mid] < nums[high]:
+        high = mid
+      else:
+        low = mid + 1
+
+    return nums[low]
