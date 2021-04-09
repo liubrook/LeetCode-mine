290dcacc8a499058a2ba7257723f2e92816ba1d5
brookliu <492525217@qq.com>
2021-04-09 09:40:53 +0800

    0154

---
+++ 0154-Find-Min-II/0154.py
@@ -0,0 +1,12 @@
+class Solution:
+  def findMin(self, nums: List[int]) -> int:
+    low, high = 0, len(nums) - 1
+    while low < high:
+      mid = low + (high - low) // 2
+      if nums[mid] < nums[high]:
+        high = mid
+      elif nums[mid] > nums[high]:
+        low = mid + 1
+      else:
+        high -= 1
+    return nums[low]
