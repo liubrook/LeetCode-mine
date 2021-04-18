b05acae6e895b12556264c960d7cfca7436f3179
brookliu <492525217@qq.com>
2021-04-18 17:08:43 +0800

    0026

---
+++ 0026-Remove-Duplicates/0026.go
@@ -0,0 +1,14 @@
+func removeDuplicates = (nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast - 1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
---
+++ 0026-Remove-Duplicates/0026.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int removeDuplicates = (int[] nums) {
+    int n = nums.length;
+    if (n == 0) {
+      return 0;
+    }
+    int fast = 1, slow = 1;
+    while (fast < n) {
+      if (nums[fast] != nums[fast - 1]) {
+        nums[slow] = nums[fast];
+        ++slow;
+      }
+      ++fast;
+    }
+    return slow;
+  }
+}
