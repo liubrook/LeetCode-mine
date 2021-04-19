f050021da0540fae6f61dffd23e01ab5592c9e72
brookliu <492525217@qq.com>
2021-04-19 10:03:26 +0800

    0027

---
+++ 0027-Remove-Element/0027.go
@@ -0,0 +1,12 @@
+func removeElement(nums []int, val int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		if nums[left] == val {
+			nums[left] = nums[right-1]
+			right--
+		} else {
+			left++
+		}
+	}
+	return left
+}
---
+++ 0027-Remove-Element/0027.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int removeElement(int[] nums. int val) {
+    int left = 0;
+    int right = nums.length;
+    while (left < right) {
+      if (nums[left] == val) {
+        nums[left] = nums[right - 1];
+        right--;
+      } else {
+        left++;
+      }
+    }
+    return left;
+  }
+}
