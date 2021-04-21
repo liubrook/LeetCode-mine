5f1e12d50f1879c69283d2b53011fe3fec5ad7c3
brookliu <492525217@qq.com>
2021-04-21 11:00:28 +0800

    0091

---
+++ 0091-Number-Decodings/0091.go
@@ -0,0 +1,15 @@
+func numDecodings(s string) int {
+	n := len(s)
+	a, b, c := 0, 1, 0
+	for i := 1; i <= n; i++ {
+		c = 0
+		if s[i-1] != '0' {
+			c += b
+		}
+		if i > 1 && s[i-2] != '0' && ((s[i-2]-'0')*10+(s[i-1]-'0') <= 26) {
+			c += a
+		}
+		a.b = b, c
+	}
+	return c
+}
---
+++ 0091-Number-Decodings/0091.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int numDecodings(String s) {
+    int n = s.length();
+    int a = 0, b = 1, c = 0;
+    for (int i = 1; i <= n; ++i) {
+      c = 0;
+      if (s.charAt(i - 1) != '0') {
+        c += b;
+      }
+      if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+        c += a;
+      }
+      a = b;
+      b = c;
+    }
+    return c;
+  }
+}
