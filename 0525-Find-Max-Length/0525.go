5d63b98c581e8baae777df05117e9d6dee79b9d4
brookliu <492525217@qq.com>
2021-06-03 09:18:34 +0800

    0525

---
+++ 0525-Find-Max-Length/0525.go
@@ -0,0 +1,24 @@
+func findMaxLength(nums []int) (maxLength int) {
+	mp := map[int]int{0: -1}
+	counter := 0
+	for i, num := range nums {
+		if num == 1 {
+			counter++
+		} else {
+			counter--
+		}
+		if prevIndex, has := mp[counter]; has {
+			maxLength = max(maxLength, i-prevIndex)
+		} else {
+			mp[counter] = i
+		}
+	}
+	return
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ 0525-Find-Max-Length/0525.java
@@ -0,0 +1,24 @@
+class Solution {
+  public int findMaxLength(int[] nums) {
+    int maxLength = 0;
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    int counter = 0;
+    map.put(counter, -1);
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      int num = nums[i];
+      if (num == 1) {
+        counter++;
+      } else {
+        counter--;
+      }
+      if (map.containsKey(counter)) {
+        int prevIndex = map.get(counter);
+        maxLength = Math.max(maxLength, i - prevIndex);
+      } else {
+        map.put(counter, i);
+      }
+    }
+    return maxLength;
+  }
+}
