c2795c11f2a98999a8a22ad40178ccd9f2493755
brookliu <492525217@qq.com>
2022-01-19 13:14:59 +0800

    0219

--- 0219-Contains-NearBy-Duplicate/0219.js
+++ 0219-Contains-NearBy-Duplicate/0219.js
@@ -1,7 +1,7 @@
 // 219. 存在重复元素 II
 // 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
 
- 
+
 
 // 示例 1:
 
@@ -16,9 +16,12 @@
 // 输入: nums = [1,2,3,1,2,3], k = 2
 // 输出: false
 
-var containsNearbyDuplicate = function(nums, k) {
+// timePoints[i] 格式为 "HH:MM"
+
+
+var containsNearbyDuplicate = function (nums, k) {
   const set = new Set()
-  for(let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (set.has(nums[i])) {
       return true
     }
