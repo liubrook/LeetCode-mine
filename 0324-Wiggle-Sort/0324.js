83e89fce4658fda4fcf655dba2bbc074b077fa85
brookliu <492525217@qq.com>
2022-06-28 09:30:34 +0800

    0324

---
+++ 0324-Wiggle-Sort/0324.js
@@ -0,0 +1,45 @@
+// 324. 摆动排序 II
+// 给你一个整数数组 nums，将它重新排列成 nums[0] < nums[1] > nums[2] < nums[3]...的顺序。
+
+// 你可以假设所有输入数组都可以得到满足题目要求的结果。
+
+
+
+// 示例 1：
+
+// 输入：nums = [1, 5, 1, 1, 6, 4]
+// 输出：[1, 6, 1, 5, 1, 4]
+// 解释：[1, 4, 1, 5, 1, 6] 同样是符合题目要求的结果，可以被判题程序接受。
+// 示例 2：
+
+// 输入：nums = [1, 3, 2, 2, 3, 1]
+// 输出：[2, 3, 1, 3, 1, 2]
+
+
+// 提示：
+
+// 1 <= nums.length <= 5 * 104
+// 0 <= nums[i] <= 5000
+// 题目数据保证，对于给定的输入 nums ，总能产生满足题目要求的结果
+
+
+// 进阶：你能用 O(n) 时间复杂度和 / 或原地 O(1) 额外空间来实现吗？
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var wiggleSort = function (nums) {
+  const arr = nums.slice();
+  arr.sort((a, b) => a - b);
+  const n = nums.length;
+  const x = Math.floor((n + 1) / 2);
+  for (let i = 0, j = x - 1, k = n - 1; i < n; i += 2, j--, k--) {
+    nums[i] = arr[j];
+    if (i + 1 < n) {
+      nums[i + 1] = arr[k];
+    }
+  }
+};
