ea000573e502f5b271dd1640054bf126b03e3c8e
brookliu <492525217@qq.com>
2023-04-24 22:54:40 +0800

    1163

---
+++ 1163-Last-Substring/1163.js
@@ -0,0 +1,43 @@
+// 1163. 按字典序排在最后的子串
+// 给你一个字符串 s ，找出它的所有子串并按字典序排列，返回排在最后的那个子串。
+
+
+
+// 示例 1：
+
+// 输入：s = "abab"
+// 输出："bab"
+// 解释：我们可以找出 7 个子串["a", "ab", "aba", "abab", "b", "ba", "bab"]。按字典序排在最后的子串是 "bab"。
+// 示例 2：
+
+// 输入：s = "leetcode"
+// 输出："tcode"
+
+
+// 提示：
+
+// 1 <= s.length <= 4 * 10^5
+// s 仅含有小写英文字符。
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var lastSubstring = function (s) {
+  let i = 0, j = 1, n = s.length;
+  while (j < n) {
+    let k = 0;
+    while (j + k < n && s[i + k] === s[j + k]) {
+      k++;
+    }
+    if (j + k < n && s[i + k] < s[j + k]) {
+      let t = i;
+      i = j;
+      j = Math.max(j + 1, t + k + 1);
+    } else {
+      j = j + k + 1;
+    }
+  }
+  return s.substring(i, n);
+};
