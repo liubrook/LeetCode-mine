0d9e8aea02dd510f507153f287dc2fdeed44fdc5
brookliu <492525217@qq.com>
2023-07-01 14:04:35 +0800

    0001

---
+++ 0001-Two-Sum/0001.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
