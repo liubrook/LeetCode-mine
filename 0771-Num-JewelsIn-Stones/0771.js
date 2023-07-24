e5168a080caf6626c85dccb473b57d78cfa155f3
brookliu <492525217@qq.com>
2023-07-24 11:52:39 +0800

    0771

---
+++ 0771-Num-JewelsIn-Stones/0771.js
@@ -0,0 +1,34 @@
+// 771. 宝石与石头
+//  给你一个字符串 jewels 代表石头中宝石的类型，另有一个字符串 stones 代表你拥有的石头。 stones 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+
+// 字母区分大小写，因此 "a" 和 "A" 是不同类型的石头。
+
+
+
+// 示例 1：
+
+// 输入：jewels = "aA", stones = "aAAbbbb"
+// 输出：3
+// 示例 2：
+
+// 输入：jewels = "z", stones = "ZZ"
+// 输出：0
+
+
+// 提示：
+
+// 1 <= jewels.length, stones.length <= 50
+// jewels 和 stones 仅由英文字母组成
+// jewels 中的所有字符都是 唯一的
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+  const jewelsSet = new Set(jewels.split(''));
+  return stones.split('').reduce((prev, val) => {
+    return prev + jewelsSet.has(val);
+  }, 0);
+};
