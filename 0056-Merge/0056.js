b4e286625c193983ceb3e88e247b70197023343f
brookliu <492525217@qq.com>
2023-08-27 10:37:34 +0800

    0056

---
+++ 0056-Merge/0056.js
@@ -0,0 +1,51 @@
+// 56. 合并区间
+// 中等
+// 2K
+// 相关企业
+// 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+
+
+// 示例 1：
+
+// 输入：intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+// 输出：[[1, 6], [8, 10], [15, 18]]
+// 解释：区间[1, 3] 和[2, 6] 重叠, 将它们合并为[1, 6].
+//   示例 2：
+
+// 输入：intervals = [[1, 4], [4, 5]]
+// 输出：[[1, 5]]
+// 解释：区间[1, 4] 和[4, 5] 可被视为重叠区间。
+
+
+// 提示：
+
+// 1 <= intervals.length <= 10^4
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104^
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  let res = [];
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let prev = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    let cur = intervals[i];
+    if (prev[1] >= cur[0]) {
+      // 有重合
+      prev[1] = Math.max(cur[1], prev[1]);
+    } else {
+      // 不重合，prev推入res数组
+      res.push(prev);
+      prev = cur; // 更新prev
+    }
+  }
+
+  res.push(prev);
+  return res;
+};
