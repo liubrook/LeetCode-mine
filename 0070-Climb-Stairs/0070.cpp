e16797d5a3c4ed0a03d7d060b65946e474ce6153
brookliu <492525217@qq.com>
2023-12-10 16:33:00 +0800

    0070

---
+++ 0070-Climb-Stairs/0070.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+  int climbStairs(int n)
+  {
+    int p = 0, q = 0, r = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+      p = q;
+      q = r;
+      r = p + q;
+    }
+    return r;
+  }
+};
