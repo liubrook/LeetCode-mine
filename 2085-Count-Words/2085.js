1521b71cf974a8dd670ef0c2ff7b225068be99d9
brookliu <492525217@qq.com>
2024-01-12 13:10:07 +0800

    2085

---
+++ 2085-Count-Words/2085.js
@@ -0,0 +1,62 @@
+// 2085. 统计出现过一次的公共字符串
+// 简单
+// 相关标签
+// 相关企业
+// 提示
+// 给你两个字符串数组 words1 和 words2 ，请你返回在两个字符串数组中 都恰好出现一次 的字符串的数目。
+
+
+
+// 示例 1：
+
+// 输入：words1 = ["leetcode", "is", "amazing", "as", "is"], words2 = ["amazing", "leetcode", "is"]
+// 输出：2
+// 解释：
+// - "leetcode" 在两个数组中都恰好出现一次，计入答案。
+// - "amazing" 在两个数组中都恰好出现一次，计入答案。
+// - "is" 在两个数组中都出现过，但在 words1 中出现了 2 次，不计入答案。
+// - "as" 在 words1 中出现了一次，但是在 words2 中没有出现过，不计入答案。
+// 所以，有 2 个字符串在两个数组中都恰好出现了一次。
+// 示例 2：
+
+// 输入：words1 = ["b", "bb", "bbb"], words2 = ["a", "aa", "aaa"]
+// 输出：0
+// 解释：没有字符串在两个数组中都恰好出现一次。
+// 示例 3：
+
+// 输入：words1 = ["a", "ab"], words2 = ["a", "a", "a", "ab"]
+// 输出：1
+// 解释：唯一在两个数组中都出现一次的字符串是 "ab" 。
+
+
+// 提示：
+
+// 1 <= words1.length, words2.length <= 1000
+// 1 <= words1[i].length, words2[j].length <= 30
+// words1[i] 和 words2[j] 都只包含小写英文字母。
+
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {number}
+ */
+var countWords = function (words1, words2) {
+  // 统计字符串出现频率
+  const freq1 = new Map();
+  const freq2 = new Map();
+  for (const w of words1) {
+    freq1.set(w, (freq1.get(w) || 0) + 1);
+  }
+  for (const w of words2) {
+    freq2.set(w, (freq2.get(w) || 0) + 1);
+  }
+
+  // 遍历 words1 出现的字符串并检查个数
+  let res = 0;
+  for (const [w, cnt1] of freq1.entries()) {
+    if (cnt1 === 1 && freq2.get(w) === 1) {
+      res++;
+    }
+  }
+  return res;
+};
