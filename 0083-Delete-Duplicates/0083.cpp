c3263818f042b41a81313ee3e43213fc4ba3c017
brookliu <492525217@qq.com>
2024-01-14 12:13:17 +0800

    0083

---
+++ 0083-Delete-Duplicates/0083.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) {
+            return head;
+        }
+
+        ListNode* cur = head;
+        while (cur->next) {
+            if (cur->val == cur->next->val) {
+                cur->next = cur->next->next;
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+
+        return head;
+    }
+};
