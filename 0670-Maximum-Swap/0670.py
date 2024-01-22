6ae533df1c3c46914e4d936024c58772222ab436
brookliu <492525217@qq.com>
2024-01-22 11:56:02 +0800

    0670

---
+++ 0670-Maximum-Swap/0670.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        ans = num
+        s = list(str(num))
+        for i in range(len(s)):
+            for j in range(i):
+                s[i], s[j] = s[j], s[i]
+                ans = max(ans, int(''.join(s)))
+                s[i], s[j] = s[j], s[i]
+        return ans
