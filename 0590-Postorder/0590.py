85260605d6a93356c8d2ed77a6491336ec4a4929
brookliu <492525217@qq.com>
2024-02-19 09:05:50 +0800

    0590

---
+++ 0590-Postorder/0590.py
@@ -0,0 +1,11 @@
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        ans = []
+        def dfs(node: 'Node'):
+            if node is None:
+                return
+            for ch in node.children:
+                dfs(ch)
+            ans.append(node.val)
+        dfs(root)
+        return ans
