49e92bc248a50f4209efb9b7c832ac96eeb09d01
brookliu <492525217@qq.com>
2024-03-04 19:17:56 +0800

    0232

--- 0232-My-Queue/0232.js
+++ 0232-My-Queue/0232.js
@@ -7,18 +7,18 @@
 // int pop() 从队列的开头移除并返回元素
 // int peek() 返回队列开头的元素
 // boolean empty() 如果队列为空，返回 true ；否则，返回 false
- 
+
 
 // 说明：
 
 // 你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
 // 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
- 
+
 
 // 进阶：
 
 // 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
- 
+
 
 // 示例：
 
@@ -35,7 +35,7 @@
 // myQueue.peek(); // return 1
 // myQueue.pop(); // return 1, queue is [2]
 // myQueue.empty(); // return false
- 
+
 
 // 提示：
 
@@ -46,7 +46,7 @@
 /**
  * Initialize your data structure here.
  */
-var MyQueue = function() {
+var MyQueue = function () {
   this.inStack = [];
   this.outStack = [];
 };
@@ -56,7 +56,7 @@ var MyQueue = function() {
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function(x) {
+MyQueue.prototype.push = function (x) {
   this.inStack.push(x);
 };
 
@@ -64,7 +64,7 @@ MyQueue.prototype.push = function(x) {
  * Removes the element from in front of queue and returns that element.
  * @return {number}
  */
-MyQueue.prototype.pop = function() {
+MyQueue.prototype.pop = function () {
   if (!this.outStack.length) {
     this.in2out();
   }
@@ -75,23 +75,23 @@ MyQueue.prototype.pop = function() {
  * Get the front element.
  * @return {number}
  */
-MyQueue.prototype.peek = function() {
+MyQueue.prototype.peek = function () {
   if (!this.outStack.length) {
     this.in2out();
   }
-  return this.outStack[this.outStack,length - 1];
+  return this.outStack[this.outStack.length - 1];
 };
 
 /**
  * Returns whether the queue is empty.
  * @return {boolean}
  */
-MyQueue.prototype.empty = function() {
-  return this,this.outStack.length === 0 && this.inStack.length === 0;
+MyQueue.prototype.empty = function () {
+  return this.outStack.length === 0 && this.inStack.length === 0;
 };
 
-MyQueue.prototype.in2out = function() {
-  while(this.inStack.length) {
+MyQueue.prototype.in2out = function () {
+  while (this.inStack.length) {
     this.outStack.push(this.inStack.pop());
   }
 }
