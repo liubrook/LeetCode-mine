f2bec8f929706bf6c41d1fccc4438f0793f7f50c
brookliu <492525217@qq.com>
2024-04-14 00:03:54 +0800

    0705

---
+++ 0705-My-Hash-Set/0705.go
@@ -0,0 +1,39 @@
+const base = 769
+
+type MyHashSet struct {
+	data []list.List
+}
+
+func Constructor() MyHashSet {
+	return MyHashSet{make([]list.List, base)}
+}
+
+func (s *MyHashSet) hash(key int) int {
+	return key % base
+}
+
+func (s *MyHashSet) Add(key int) {
+	if !s.Contains(key) {
+		h := s.hash(key)
+		s.data[h].PushBack(key)
+	}
+}
+
+func (s *MyHashSet) Remove(key int) {
+	h := s.hash(key)
+	for e := s.data[h].Front(); e != nil; e = e.Next() {
+		if e.Value.(int) == key {
+			s.data[h].Remove(e)
+		}
+	}
+}
+
+func (s *MyHashSet) Contains(key int) bool {
+	h := s.hash(key)
+	for e := s.data[h].Front(); e != nil; e = e.Next() {
+		if e.Value.(int) == key {
+			return true
+		}
+	}
+	return false
+}
