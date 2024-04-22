8cf5287cad7fd3d7545bea513c770c3c0fdb03ee
brookliu <492525217@qq.com>
2024-04-23 00:06:13 +0800

    1052

---
+++ 1052-Max-Satisfied/1052.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int total = 0;
+        int n = customers.length;
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0) {
+                total += customers[i];
+            }
+        }
+        int increase = 0;
+        for (int i = 0; i < minutes; i++) {
+            increase += customers[i] * grumpy[i];
+        }
+        int maxIncrease = increase;
+        for (int i = minutes; i < n; i++) {
+            increase = increase - customers[i - minutes] * grumpy[i - minutes] + customers[i] * grumpy[i];
+            maxIncrease = Math.max(maxIncrease, increase);
+        }
+        return total + maxIncrease;
+    }
+}
