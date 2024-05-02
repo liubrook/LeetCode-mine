4d897c8f5e2699937d966ba9405d26ce2dc077fe
brookliu <492525217@qq.com>
2024-05-02 08:48:26 +0800

    0857

---
+++ 0857-Mincost-To-Hire-Workers/0857.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        pairs = sorted(zip(quality, wage), key=lambda p: p[1] / p[0])
+        ans = inf
+        totalq = 0
+        h = []
+        for q, w in pairs[:k - 1]:
+            totalq += q
+            heappush(h, -q)
+        for q, w in pairs[k - 1:]:
+            totalq += q
+            heappush(h, -q)
+            ans = min(ans, w / q * totalq)
+            totalq += heappop(h)
+        return ans
