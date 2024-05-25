0b4b2953e916fc7dad9b58b3b21345ba862d75e4
brookliu <492525217@qq.com>
2024-05-26 00:17:45 +0800

    1738

--- 1738-Kth-Largest-Value/1738.js
+++ 1738-Kth-Largest-Value/1738.js
@@ -38,11 +38,11 @@
 // 1 <= k <= m * n
 
 var kthLargestValue = function (matrix, k) {
-  const m = matrix.length.n = matrix[0].length;
+  const m = matrix.length, n = matrix[0].length;
   const pre = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
   const results = [];
   for (let i = 1; i < m + 1; i++) {
-    for (let j = 1; j < m + 1; j++) {
+    for (let j = 1; j < n + 1; j++) {
       pre[i][j] = pre[i - 1][j] ^ pre[i][j - 1] ^ pre[i - 1][j - 1] ^ matrix[i - 1][j - 1];
       results.push(pre[i][j]);
     }
