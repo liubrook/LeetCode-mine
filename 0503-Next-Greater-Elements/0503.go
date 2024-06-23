0fc4be07755a41cf8c7d1ef565feebdbbcd16a55
brookliu <492525217@qq.com>
2024-06-24 00:03:49 +0800

    0503

---
+++ 0503-Next-Greater-Elements/0503.go
@@ -0,0 +1,16 @@
+func nextGreaterElements(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n)
+	for i := range ans {
+		ans[i] = -1
+	}
+	stack := []int{}
+	for i := 0; i < n*2-1; i++ {
+		for len(stack) > 0 && nums[stack[len(stack)-1]] < nums[i%n] {
+			ans[stack[len(stack)-1]] = nums[i%n]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i%n)
+	}
+	return ans
+}
