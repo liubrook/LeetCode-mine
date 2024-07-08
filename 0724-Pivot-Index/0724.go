765d37e96843d7b16d9e9fe4c9e224f9fdbd98a7
brookliu <492525217@qq.com>
2024-07-08 09:33:19 +0800

    0724

---
+++ 0724-Pivot-Index/0724.go
@@ -0,0 +1,14 @@
+func pivotIndex(nums []int) int {
+	total := 0
+	for _, v := range nums {
+		total += v
+	}
+	sum := 0
+	for i, v := range nums {
+		if 2*sum+v == total {
+			return i
+		}
+		sum += v
+	}
+	return -1
+}
