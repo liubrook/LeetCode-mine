f896b48ad5c16c5e2c2d1e0124b9beabae901b77
brookliu <492525217@qq.com>
2024-08-25 00:07:12 +0800

    0698

---
+++ 0698-Can-Partition-K-Subsets/0698.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        all = sum(nums)
+        if all % k:
+            return False
+        per = all // k
+        nums.sort()
+        if nums[-1] > per:
+            return False
+        n = len(nums)
+        dp = [False] * (1 << n)
+        dp[0] = True
+        cursum = [0] * (1 << n)
+        for i in range(0, 1 << n):
+            if not dp[i]:
+                continue
+            for j in range(n):
+                if cursum[i] + nums[j] > per:
+                    break
+                if (i >> j & 1) == 0:
+                    next = i | (1 << j)
+                    if not dp[next]:
+                        cursum[next] = (cursum[i] + nums[j]) % per
+                        dp[next] = True
+        return dp[(1 << n) - 1]
