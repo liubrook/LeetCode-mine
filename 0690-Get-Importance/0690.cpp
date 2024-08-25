0be0d532bfd3c0fdeda72a051a33019fa708105d
brookliu <492525217@qq.com>
2024-08-26 00:08:07 +0800

    0690

---
+++ 0690-Get-Importance/0690.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    unordered_map<int, Employee *> mp;
+
+    int dfs(int id) {
+        Employee *employee = mp[id];
+        int total = employee->importance;
+        for (int subId : employee->subordinates) {
+            total += dfs(subId);
+        }
+        return total;
+    }
+
+    int getImportance(vector<Employee *> employees, int id) {
+        for (auto &employee : employees) {
+            mp[employee->id] = employee;
+        }
+        return dfs(id);
+    }
+};
