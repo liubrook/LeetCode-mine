81579fb652b75f3e7e37cf58202763a8a49467b7
brookliu <492525217@qq.com>
2024-09-22 00:04:40 +0800

    0997

---
+++ 0997-Find-Judge/0997.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        inDegrees = Counter(y for _, y in trust)
+        outDegrees = Counter(x for x, _ in trust)
+        return next((i for i in range(1, n + 1) if inDegrees[i] == n - 1 and outDegrees[i] == 0), -1)
