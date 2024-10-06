3a96a1604218ae1aae5464bab0d2977453087c53
brookliu <492525217@qq.com>
2024-10-07 00:05:27 +0800

    0871

---
+++ 0871-Min-Refuel-Stops/0871.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        dp = [startFuel] + [0] * len(stations)
+        for i, (pos, fuel) in enumerate(stations):
+            for j in range(i, -1, -1):
+                if dp[j] >= pos:
+                    dp[j + 1] = max(dp[j + 1], dp[j] + fuel)
+        return next((i for i, v in enumerate(dp) if v >= target), -1)
