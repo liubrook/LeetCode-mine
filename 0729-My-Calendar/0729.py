2e58a56313a4f0341404dd2ce560a9f8cd6cd455
Albert <492525217@qq.com>
2025-01-02 00:03:46 +0800

    0729

---
+++ 0729-My-Calendar/0729.py
@@ -0,0 +1,35 @@
+class MyCalendar:
+    def __init__(self):
+        self.tree = set()
+        self.lazy = set()
+
+    def query(self, start: int, end: int, l: int, r: int, idx: int) -> bool:
+        if r < start or end < l:
+            return False
+        if idx in self.lazy:  # 如果该区间已被预订，则直接返回
+            return True
+        if start <= l and r <= end:
+            return idx in self.tree
+        mid = (l + r) // 2
+        return self.query(start, end, l, mid, 2 * idx) or \
+               self.query(start, end, mid + 1, r, 2 * idx + 1)
+
+    def update(self, start: int, end: int, l: int, r: int, idx: int) -> None:
+        if r < start or end < l:
+            return
+        if start <= l and r <= end:
+            self.tree.add(idx)
+            self.lazy.add(idx)
+        else:
+            mid = (l + r) // 2
+            self.update(start, end, l, mid, 2 * idx)
+            self.update(start, end, mid + 1, r, 2 * idx + 1)
+            self.tree.add(idx)
+            if 2 * idx in self.lazy and 2 * idx + 1 in self.lazy:
+                self.lazy.add(idx)
+
+    def book(self, start: int, end: int) -> bool:
+        if self.query(start, end - 1, 0, 10 ** 9, 1):
+            return False
+        self.update(start, end - 1, 0, 10 ** 9, 1)
+        return True
