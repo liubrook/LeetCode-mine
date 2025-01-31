d74a46b3beffce4cfac8c2dc80d9c3c72c66f67f
Albert <492525217@qq.com>
2025-01-31 17:01:37 +0800

    0541

---
+++ 0541-Reverse-Str/0541.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        t = list(s)
+        for i in range(0, len(t), 2 * k):
+            t[i: i + k] = reversed(t[i: i + k])
+        return "".join(t)
