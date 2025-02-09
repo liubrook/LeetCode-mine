484091561beeef61a6435244c61b2bf4dc8b1fe4
Albert <492525217@qq.com>
2025-02-10 00:04:49 +0800

    0913

--- 0913-Cat-Mouse-Game/0913.js
+++ 0913-Cat-Mouse-Game/0913.js
@@ -46,51 +46,75 @@
  * @param {number[][]} graph
  * @return {number}
  */
-const MOUSE_WIN = 1;
-const CAT_WIN = 2;
-const DRAW = 0;
-
+const MOUSE_TURN = 0, CAT_TURN = 1;
+const DRAW = 0, MOUSE_WIN = 1, CAT_WIN = 2;
 var catMouseGame = function (graph) {
-  this.n = graph.length;
-  this.graph = graph;
-  this.dp = new Array(n).fill(0).map(() => new Array(n).fill(0).map(() => new Array(n * 2).fill(-1)));
-  return getResult(1, 2, 0);
-};
-
-const getResult = (mouse, cat, turns) => {
-  if (turns === n * 2) {
-    return DRAW;
-  }
-  if (dp[mouse][cat][turns] < 0) {
-    if (mouse === 0) {
-      dp[mouse][cat][turns] = MOUSE_WIN;
-    } else if (cat === mouse) {
-      dp[mouse][cat][turns] = CAT_WIN;
-    } else {
-      getNextResult(mouse, cat, turns);
+  const n = graph.length;
+  degrees = new Array(n).fill(0).map(() => new Array(n).fill(0).map(() => new Array(2).fill(0)));
+  results = new Array(n).fill(0).map(() => new Array(n).fill(0).map(() => new Array(2).fill(0)));
+  const queue = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 1; j < n; j++) {
+      degrees[i][j][MOUSE_TURN] = graph[i].length;
+      degrees[i][j][CAT_TURN] = graph[j].length;
     }
   }
-  return dp[mouse][cat][turns];
-}
-
-const getNextResult = (mouse, cat, turns) => {
-  const curMove = turns % 2 === 0 ? mouse : cat;
-  const defaultResult = curMove == mouse ? CAT_WIN : MOUSE_WIN;
-  let result = defaultResult;
-  const nextNodes = graph[curMove];
-  for (const next of nextNodes) {
-    if (curMove === cat && next === 0) {
-      continue;
+  for (const node of graph[0]) {
+    for (let i = 0; i < n; i++) {
+      degrees[i][node][CAT_TURN]--;
     }
-    const nextMouse = curMove == mouse ? next : mouse;
-    const nextCat = curMove == cat ? next : cat;
-    const nextResult = getResult(nextMouse, nextCat, turns + 1);
-    if (nextResult !== defaultResult) {
-      result = nextResult;
-      if (result !== DRAW) {
-        break;
+  }
+  for (let j = 1; j < n; j++) {
+    results[0][j][MOUSE_TURN] = MOUSE_WIN;
+    results[0][j][CAT_TURN] = MOUSE_WIN;
+    queue.push([0, j, MOUSE_TURN]);
+    queue.push([0, j, CAT_TURN]);
+  }
+  for (let i = 1; i < n; i++) {
+    results[i][i][MOUSE_TURN] = CAT_WIN;
+    results[i][i][CAT_TURN] = CAT_WIN;
+    queue.push([i, i, MOUSE_TURN]);
+    queue.push([i, i, CAT_TURN]);
+  }
+  while (queue.length) {
+    const state = queue.shift();
+    const mouse = state[0], cat = state[1], turn = state[2];
+    const result = results[mouse][cat][turn];
+    const prevStates = getPrevStates(mouse, cat, turn, graph);
+    for (const prevState of prevStates) {
+      let prevMouse = prevState[0], prevCat = prevState[1], prevTurn = prevState[2];
+      if (results[prevMouse][prevCat][prevTurn] === DRAW) {
+        const canWin = (result === MOUSE_WIN && prevTurn === MOUSE_TURN) || (result === CAT_WIN && prevTurn === CAT_TURN);
+        if (canWin) {
+          results[prevMouse][prevCat][prevTurn] = result;
+          queue.push([prevMouse, prevCat, prevTurn]);
+        } else {
+          degrees[prevMouse][prevCat][prevTurn]--;
+          if (degrees[prevMouse][prevCat][prevTurn] == 0) {
+            const loseResult = prevTurn === MOUSE_TURN ? CAT_WIN : MOUSE_WIN;
+            results[prevMouse][prevCat][prevTurn] = loseResult;
+            queue.push([prevMouse, prevCat, prevTurn]);
+          }
+        }
       }
     }
   }
-  dp[mouse][cat][turns] = result;
+  return results[1][2][MOUSE_TURN];
+};
+
+const getPrevStates = (mouse, cat, turn, graph) => {
+  const prevStates = [];
+  const prevTurn = turn == MOUSE_TURN ? CAT_TURN : MOUSE_TURN;
+  if (prevTurn === MOUSE_TURN) {
+    for (const prev of graph[mouse]) {
+      prevStates.push([prev, cat, prevTurn]);
+    }
+  } else {
+    for (const prev of graph[cat]) {
+      if (prev != 0) {
+        prevStates.push([mouse, prev, prevTurn]);
+      }
+    }
+  }
+  return prevStates;
 }
---
+++ 0913-Cat-Mouse-Game/0913.py
@@ -0,0 +1,53 @@
+MOUSE_TURN = 0
+CAT_TURN = 1
+
+DRAW = 0
+MOUSE_WIN = 1
+CAT_WIN = 2
+
+class Solution:
+    def catMouseGame(self, graph: List[List[int]]) -> int:
+        n = len(graph)
+        degrees = [[[0, 0] for _ in range(n)] for _ in range(n)]
+        results = [[[0, 0] for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(1, n):
+                degrees[i][j][MOUSE_TURN] = len(graph[i])
+                degrees[i][j][CAT_TURN] = len(graph[j])
+        for y in graph[0]:
+            for i in range(n):
+                degrees[i][y][CAT_TURN] -= 1
+
+        q = deque()
+        for j in range(1, n):
+            results[0][j][MOUSE_TURN] = MOUSE_WIN
+            results[0][j][CAT_TURN] = MOUSE_WIN
+            q.append((0, j, MOUSE_TURN))
+            q.append((0, j, CAT_TURN))
+        for i in range(1, n):
+            results[i][i][MOUSE_TURN] = CAT_WIN
+            results[i][i][CAT_TURN] = CAT_WIN
+            q.append((i, i, MOUSE_TURN))
+            q.append((i, i, CAT_TURN))
+
+        while q:
+            mouse, cat, turn = q.popleft()
+            result = results[mouse][cat][turn]
+            if turn == MOUSE_TURN:
+                prevStates = [(mouse, prev, CAT_TURN) for prev in graph[cat]]
+            else:
+                prevStates = [(prev, cat, MOUSE_TURN) for prev in graph[mouse]]
+            for prevMouse, prevCat, prevTurn in prevStates:
+                if prevCat == 0:
+                    continue
+                if results[prevMouse][prevCat][prevTurn] == DRAW:
+                    canWin = result == MOUSE_WIN and prevTurn == MOUSE_TURN or result == CAT_WIN and prevTurn == CAT_TURN
+                    if canWin:
+                        results[prevMouse][prevCat][prevTurn] = result
+                        q.append((prevMouse, prevCat, prevTurn))
+                    else:
+                        degrees[prevMouse][prevCat][prevTurn] -= 1
+                        if degrees[prevMouse][prevCat][prevTurn] == 0:
+                            results[prevMouse][prevCat][prevTurn] = CAT_WIN if prevTurn == MOUSE_TURN else MOUSE_WIN
+                            q.append((prevMouse, prevCat, prevTurn))
+        return results[1][2][MOUSE_TURN]
