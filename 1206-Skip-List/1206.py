b1bf0377ad3711cc0519bf2af0e499163ed35dd4
Albert <492525217@qq.com>
2025-02-23 00:02:38 +0800

    1206

---
+++ 1206-Skip-List/1206.py
@@ -0,0 +1,67 @@
+MAX_LEVEL = 32
+P_FACTOR = 0.25
+
+def random_level() -> int:
+    lv = 1
+    while lv < MAX_LEVEL and random.random() < P_FACTOR:
+        lv += 1
+    return lv
+
+class SkiplistNode:
+    __slots__ = 'val', 'forward'
+
+    def __init__(self, val: int, max_level=MAX_LEVEL):
+        self.val = val
+        self.forward = [None] * max_level
+
+class Skiplist:
+    def __init__(self):
+        self.head = SkiplistNode(-1)
+        self.level = 0
+
+    def search(self, target: int) -> bool:
+        curr = self.head
+        for i in range(self.level - 1, -1, -1):
+            # 找到第 i 层小于且最接近 target 的元素
+            while curr.forward[i] and curr.forward[i].val < target:
+                curr = curr.forward[i]
+        curr = curr.forward[0]
+        # 检测当前元素的值是否等于 target
+        return curr is not None and curr.val == target
+
+    def add(self, num: int) -> None:
+        update = [self.head] * MAX_LEVEL
+        curr = self.head
+        for i in range(self.level - 1, -1, -1):
+            # 找到第 i 层小于且最接近 num 的元素
+            while curr.forward[i] and curr.forward[i].val < num:
+                curr = curr.forward[i]
+            update[i] = curr
+        lv = random_level()
+        self.level = max(self.level, lv)
+        new_node = SkiplistNode(num, lv)
+        for i in range(lv):
+            # 对第 i 层的状态进行更新，将当前元素的 forward 指向新的节点
+            new_node.forward[i] = update[i].forward[i]
+            update[i].forward[i] = new_node
+
+    def erase(self, num: int) -> bool:
+        update = [None] * MAX_LEVEL
+        curr = self.head
+        for i in range(self.level - 1, -1, -1):
+            # 找到第 i 层小于且最接近 num 的元素
+            while curr.forward[i] and curr.forward[i].val < num:
+                curr = curr.forward[i]
+            update[i] = curr
+        curr = curr.forward[0]
+        if curr is None or curr.val != num:  # 值不存在
+            return False
+        for i in range(self.level):
+            if update[i].forward[i] != curr:
+                break
+            # 对第 i 层的状态进行更新，将 forward 指向被删除节点的下一跳
+            update[i].forward[i] = curr.forward[i]
+        # 更新当前的 level
+        while self.level > 1 and self.head.forward[self.level - 1] is None:
+            self.level -= 1
+        return True
