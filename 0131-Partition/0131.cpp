212e168635b74211377ec071392dac8967c8e2a3
Albert <492525217@qq.com>
2025-03-01 00:02:43 +0800

    0131

---
+++ 0131-Partition/0131.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    vector<vector<int>> f;
+    vector<vector<string>> ret;
+    vector<string> ans;
+    int n;
+
+public:
+    void dfs(const string& s, int i) {
+        if (i == n) {
+            ret.push_back(ans);
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (isPalindrome(s, i, j) == 1) {
+                ans.push_back(s.substr(i, j - i + 1));
+                dfs(s, j + 1);
+                ans.pop_back();
+            }
+        }
+    }
+
+    // 记忆化搜索中，f[i][j] = 0 表示未搜索，1 表示是回文串，-1 表示不是回文串
+    int isPalindrome(const string& s, int i, int j) {
+        if (f[i][j]) {
+            return f[i][j];
+        }
+        if (i >= j) {
+            return f[i][j] = 1;
+        }
+        return f[i][j] = (s[i] == s[j] ? isPalindrome(s, i + 1, j - 1) : -1);
+    }
+
+    vector<vector<string>> partition(string s) {
+        n = s.size();
+        f.assign(n, vector<int>(n));
+
+        dfs(s, 0);
+        return ret;
+    }
+};
