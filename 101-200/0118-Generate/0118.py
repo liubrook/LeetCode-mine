4be4be8767dc5f9139948a6143daf1d5ab972ffe
Albert <492525217@qq.com>
2025-08-01 01:07:27 +0800

    0118

---
+++ 101-200/0118-Generate/0118.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ret = list()
+        for i in range(numRows):
+            row = list()
+            for j in range(0, i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(ret[i - 1][j] + ret[i - 1][j - 1])
+            ret.append(row)
+        return ret
