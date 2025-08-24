4049fc1bdcd331f9e4748d1e3da89792ec87e2cf
Albert <492525217@qq.com>
2025-08-25 00:02:31 +0800

    0498

---
+++ 401-500/0498-Find-Diagonal-Order/0498.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        ans = []
+        m, n = len(mat), len(mat[0])
+        for i in range(m + n - 1):
+            if i % 2:
+                x = 0 if i < n else i - n + 1
+                y = i if i < n else n - 1
+                while x < m and y >= 0:
+                    ans.append(mat[x][y])
+                    x += 1
+                    y -= 1
+            else:
+                x = i if i < m else m - 1
+                y = 0 if i < m else i - m + 1
+                while x >= 0 and y < n:
+                    ans.append(mat[x][y])
+                    x -= 1
+                    y += 1
+        return ans
