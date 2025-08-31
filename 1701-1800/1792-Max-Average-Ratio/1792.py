cd705366e6accea57669bbb0439b3c40c6998e78
Albert <492525217@qq.com>
2025-09-01 00:03:46 +0800

    1792

---
+++ 1701-1800/1792-Max-Average-Ratio/1792.py
@@ -0,0 +1,17 @@
+class Entry:
+    __slots__ = 'p', 't'
+
+    def __init__(self, p: int, t: int):
+        self.p = p
+        self.t = t
+
+    def __lt__(self, b: 'Entry') -> bool:
+        return (self.t - self.p) * b.t * (b.t + 1) > (b.t - b.p) * self.t * (self.t + 1)
+
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        h = [Entry(*c) for c in classes]
+        heapify(h)
+        for _ in range(extraStudents):
+            heapreplace(h, Entry(h[0].p + 1, h[0].t + 1))
+        return sum(e.p / e.t for e in h) / len(h)
