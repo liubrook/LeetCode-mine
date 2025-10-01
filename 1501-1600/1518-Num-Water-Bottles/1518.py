3508d505c21b476a3fbe6949d1943aaf280fcc26
Albert <492525217@qq.com>
2025-10-01 19:36:51 +0800

    1518

---
+++ 1501-1600/1518-Num-Water-Bottles/1518.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        bottle, ans = numBottles, numBottles
+        while bottle >= numExchange:
+            bottle -= numExchange
+            ans += 1
+            bottle += 1
+        return ans
