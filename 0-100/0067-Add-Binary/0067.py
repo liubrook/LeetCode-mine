7aaa2e469f28f1d7ee44d4b55e876ea834899636
liubrook <492525217@qq.com>
2026-02-15 09:20:35 +0800

    0067

---
+++ 0-100/0067-Add-Binary/0067.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
