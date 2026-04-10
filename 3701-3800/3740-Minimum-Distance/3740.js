0614f1a978c9e6945531f99dc30e88dcd2420d2b
liubrook <492525217@qq.com>
2026-04-10 11:18:16 +0800

    3740

---
+++ 3701-3800/3740-Minimum-Distance/3740.js
@@ -0,0 +1,80 @@
+// 3740. 三个相等元素之间的最小距离 I
+// 简单
+// 相关标签
+// premium lock icon
+// 相关企业
+// 提示
+// 给你一个整数数组 nums。
+
+// 如果满足 nums[i] == nums[j] == nums[k]，且 (i, j, k) 是 3 个 不同 下标，那么三元组 (i, j, k) 被称为 有效三元组 。
+
+// 有效三元组 的 距离 被定义为 abs(i - j) + abs(j - k) + abs(k - i)，其中 abs(x) 表示 x 的 绝对值 。
+
+// 返回一个整数，表示 有效三元组 的 最小 可能距离。如果不存在 有效三元组 ，返回 -1。
+
+// 示例 1：
+
+// 输入： nums = [1,2,1,1,3]
+
+// 输出： 6
+
+// 解释：
+
+// 最小距离对应的有效三元组是 (0, 2, 3) 。
+
+// (0, 2, 3) 是一个有效三元组，因为 nums[0] == nums[2] == nums[3] == 1。它的距离为 abs(0 - 2) + abs(2 - 3) + abs(3 - 0) = 2 + 1 + 3 = 6。
+
+// 示例 2：
+
+// 输入： nums = [1,1,2,3,2,1,2]
+
+// 输出： 8
+
+// 解释：
+
+// 最小距离对应的有效三元组是 (2, 4, 6) 。
+
+// (2, 4, 6) 是一个有效三元组，因为 nums[2] == nums[4] == nums[6] == 2。它的距离为 abs(2 - 4) + abs(4 - 6) + abs(6 - 2) = 2 + 2 + 4 = 8。
+
+// 示例 3：
+
+// 输入： nums = [1]
+
+// 输出： -1
+
+// 解释：
+
+// 不存在有效三元组，因此答案为 -1。
+
+// 提示：
+
+// 1 <= n == nums.length <= 100
+// 1 <= nums[i] <= n
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDistance = function (nums) {
+  let res = Infinity;
+  const n = nums.length;
+  const cache = Array.from({ length: n + 1 }, () => []);
+
+  for (let i = 0; i < n; i++) {
+    const num = nums[i];
+    const arr = cache[num];
+
+    if (arr.length < 3) {
+      arr.push(i);
+    } else {
+      arr[0] = arr[1];
+      arr[1] = arr[2];
+      arr[2] = i;
+    }
+
+    if (arr.length === 3) {
+      res = Math.min(res, 2 * (arr[2] - arr[0]));
+    }
+  }
+
+  return res === Infinity ? -1 : res;
+};
