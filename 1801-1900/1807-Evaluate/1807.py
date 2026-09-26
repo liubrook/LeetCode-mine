2901d65ec39c12c50dadf27f010ec7a251a6dcc1
liubrook <492525217@qq.com>
2026-09-26 08:54:21 +0800

    1807

---
+++ 1801-1900/1807-Evaluate/1807.py
@@ -0,0 +1,14 @@
+class Solution:
+    def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
+        d = dict(knowledge)
+        ans, start = [], -1
+        for i, c in enumerate(s):
+            if c == '(':
+                start = i
+            elif c == ')':
+                ans.append(d.get(s[start + 1: i], '?'))
+                start = -1
+            elif start < 0:
+                ans.append(c)
+        return "".join(ans)
+    
